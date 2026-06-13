985f30348cff5a941a53f9e8dc879bfb67e56a51
Kushaaal12 <kushalsharma121006@gmail.com>
2026-06-14 01:32:53 +0530

    Week 2 Day 5

---
+++ Week_2/Day_5/443_StringCompression.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int compress(char[] chars) {
+        int n = chars.length;
+        int write = 0;  
+        int read = 0;    
+        while (read < n) {
+            char current = chars[read];
+            int count = 0;
+            while (read < n && chars[read] == current) {
+                read++;
+                count++;
+            }
+            chars[write++] = current;   
+            if (count > 1) {
+                String s = String.valueOf(count);
+                for (char c : s.toCharArray()) {
+                    chars[write++] = c;
+                }
+            }
+        }
+        return write;
+    }
+}
